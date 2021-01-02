de533bb99faeb7bd20b62f46e4e3393766f06c1b
Miguel Bernal <miguel1226bernal@gmail.com>
2021-01-02 14:18:47 -0500

    Solicitudes get, post, put y delete agregados

--- routes/movies.js
+++ routes/movies.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var Movie = require('../models/Movie.js');
 
-/* GET users listing. */
+/* GET the movies listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  Movie.find().sort('year').exec(function(err, movies){
+    if (err) res.status(500).send(err);
+    else res.status(200).json(movies);
+  })
+});
+
+/* GET the movie identified by id */
+router.get('/:id', function(req, res, next) {
+  Movie.findById(req.params.id, function(err, movieinfo){
+    if (err) res.status(500).send(err);
+    else res.status(200).json(movieinfo);
+  })
+});
+
+/* POST the movies listing. */
+router.post('/', function(req, res, next) {
+  Movie.create(req.body, function(err, movieinfo){
+    if (err) res.status(500).send(err);
+    else res.sendStatus(200);
+  })
+});
+
+/* PUT movie identified by id*/
+router.put('/:id', function(req, res, next) {
+  Movie.findByIdAndUpdate(req.params.id, req.body, function (err, movieinfo){
+    if (err) res.status(500).send(err);
+    else res.sendStatus(200);
+  })
+});
+
+/* Delete movie identified by id*/
+router.delete('/:id', function(req, res, next) {
+  res.send('delete movie ' + req.params.id);
 });
 
 module.exports = router;
